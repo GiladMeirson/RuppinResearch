49f2b551c92fe52483d8e2bd88b541b27c0bc017
Gilad Meirson <gilad.meirson@gmail.com>
2025-02-11 15:31:30 +0200

    add temrs use and footer dinamic

---
+++ public/master.js
@@ -0,0 +1,14 @@
+const footerHtml=()=>{
+   $('body').append(
+`   <footer>
+        <div class="footer-content">
+          <p>&copy; ${new Date().getFullYear()} <a target="_blank" href="https://www.linkedin.com/in/benny-bornfeld-phd-4b6357?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BwxMoKMZAS%2F%2BDfldt8AcJ4w%3D%3D">Benny Bornfeld Ph.D.</a>  created by <a target="_blank" href="https://giladmeirson.site/">Gilad Meirson B.Sc.</a>  All rights reserved.</p>
+          <nav class="footerNav">
+            <a href="./bir/terms.html">Terms of Use</a>
+            <a href="#">Contact Us</a>
+          </nav>
+        </div>
+    </footer>`
+
+   )
+}
